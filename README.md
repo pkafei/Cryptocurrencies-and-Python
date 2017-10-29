cf532650c75d2f9ecdb83c958bc99297056f59d0
Portia Burton <fengmi@Portias-MacBook-Pro.local>
2017-10-29 10:36:35 -0400

    Initial Commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Python and Cryptocurrencies
+
+#### Code for the [The Python and Cryptocurrencies webinar](https://youtu.be/4J-RCUer6BE)
+
+
+### Setting up Dev Environment
+
+
+1. Download the Python data science packages via [Anaconda](https://www.anaconda.com/download/#macos)
+
+2. Create a virtual environment for your projects
+
+`conda create --name cryptocurrency-analysis python=3`
+
+
+3. Start you virtual environment `source activate cryptocurrency-analysis`
+
+4. Install of your dependencies `conda install numpy pandas nb_conda jupyter plotly quandl`
+
+5. Start the notebook by running `jupyter notebook` in the command line
+
+![](https://media.giphy.com/media/3o7abBP0nMjrdIvaCY/giphy.gif)
---
+++ bitcoin_price.py
@@ -0,0 +1,102 @@
+import os
+import numpy as np
+import pandas as pd
+import pickle
+import quandl
+from datetime import datetime
+
+
+import plotly.offline as py
+import plotly.graph_objs as go
+import plotly.figure_factory as ff
+py.init_notebook_mode(connected=True)
+
+def get_quandl_data(quandl_id):
+    '''Download and cache Quandl dataseries'''
+    cache_path = '{}.pkl'.format(quandl_id).replace('/','-')
+    try:
+        f = open(cache_path, 'rb')
+        df = pickle.load(f)   
+        print('Loaded {} from cache'.format(quandl_id))
+    except (OSError, IOError) as e:
+        print('Downloading {} from Quandl'.format(quandl_id))
+        df = quandl.get(quandl_id, returns="pandas")
+        df.to_pickle(cache_path)
+        print('Cached {} at {}'.format(quandl_id, cache_path))
+    return df
+
+# Pull Kraken BTC price exchange data
+btc_usd_price_kraken = get_quandl_data('BCHARTS/KRAKENUSD')
+
+# Pull pricing data for 3 more BTC exchanges
+exchanges = ['COINBASE','BITSTAMP','ITBIT']
+
+exchange_data = {}
+
+exchange_data['KRAKEN'] = btc_usd_price_kraken
+
+for exchange in exchanges:
+    exchange_code = 'BCHARTS/{}USD'.format(exchange)
+    btc_exchange_df = get_quandl_data(exchange_code)
+    exchange_data[exchange] = btc_exchange_df
+
+def merge_dfs_on_column(dataframes, labels, col):
+    '''Merge a single column of each dataframe into a new combined dataframe'''
+    series_dict = {}
+    for index in range(len(dataframes)):
+        series_dict[labels[index]] = dataframes[index][col]
+        
+    return pd.DataFrame(series_dict)
+
+# Merge the BTC price dataseries' into a single dataframe
+btc_usd_datasets = merge_dfs_on_column(list(exchange_data.values()), list(exchange_data.keys()), 'Weighted Price')
+
+def df_scatter(df, title, seperate_y_axis=False, y_axis_label='', scale='linear', initial_hide=False):
+    '''Generate a scatter plot of the entire dataframe'''
+    label_arr = list(df)
+    series_arr = list(map(lambda col: df[col], label_arr))
+    
+    layout = go.Layout(
+        title=title,
+        legend=dict(orientation="h"),
+        xaxis=dict(type='date'),
+        yaxis=dict(
+            title=y_axis_label,
+            showticklabels= not seperate_y_axis,
+            type=scale
+        )
+    )
+    
+    y_axis_config = dict(
+        overlaying='y',
+        showticklabels=False,
+        type=scale )
+    
+    visibility = 'visible'
+    if initial_hide:
+        visibility = 'legendonly'
+        
+    # Form Trace For Each Series
+    trace_arr = []
+    for index, series in enumerate(series_arr):
+        trace = go.Scatter(
+            x=series.index, 
+            y=series, 
+            name=label_arr[index],
+            visible=visibility
+        )
+        
+        # Add seperate axis for the series
+        if seperate_y_axis:
+            trace['yaxis'] = 'y{}'.format(index + 1)
+            layout['yaxis{}'.format(index + 1)] = y_axis_config    
+        trace_arr.append(trace)
+
+    fig = go.Figure(data=trace_arr, layout=layout)
+    py.iplot(fig)
+
+# Calculate the average BTC price as a new column
+btc_usd_datasets['avg_btc_price_usd'] = btc_usd_datasets.mean(axis=1)
+
+
+
